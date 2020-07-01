bd603a911305a19297731df50e728720b9bece37
yasminadaileh1 <runner@fv-az70.slpfdqmc3viehlcboaza4rrate.bx.internal.cloudapp.net>
2020-07-01 11:35:35 +0000

    Updates

---
+++ precache-manifest.38325429acd5d01b9123575092290c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42bc53d20c6763fd8804b8986df5f28",
+    "url": "/resty-app/index.html"
+  },
+  {
+    "revision": "9f482ffa921fa30bf87e",
+    "url": "/resty-app/static/css/main.a3d3cc20.chunk.css"
+  },
+  {
+    "revision": "377c081def6523acd1a8",
+    "url": "/resty-app/static/js/2.3e8f73e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty-app/static/js/2.3e8f73e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f482ffa921fa30bf87e",
+    "url": "/resty-app/static/js/main.ff2976dd.chunk.js"
+  },
+  {
+    "revision": "e494b8155249ef3f58ea",
+    "url": "/resty-app/static/js/runtime-main.f3a69a2c.js"
+  }
+]);
